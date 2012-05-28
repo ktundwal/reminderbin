26da8d5f272312226078ae8587bf4cc6886ee63e
Kapil Tundwal <ktundwal@gmail.com>
2012-05-28 13:39:01 -0600

    add common.py to prod.py and removed DATABASE field

--- reminderbin/settings/prod.py
+++ reminderbin/settings/prod.py
@@ -1,18 +1,8 @@
 # Load defaults in order to then add/override with production-only settings
 import os
 from reminderbin.settings import *
+from common import *
 
 DEBUG = False
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT, 'db') + '/development.db', # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # ... etc.
